8dcba1df6a89f008d5821178ceac0b4047c99532
powerfulbean <samwisedou@outlook.com>
2021-02-20 22:38:02 -0500

    Update

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ Created on Sat Aug 31 00:52:16 2019
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("./README.md", "r") as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name="StellarInfra",
-  version="0.0.4",
+  name="StellarBrainwav",
+  version="0.0.1",
   author="Powerfulbean",
   author_email="powerfulbean@gmail.com",
-  description="An python infrastructure of StellarBlocks's software",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/StellarBlocks/StellarInfra",
