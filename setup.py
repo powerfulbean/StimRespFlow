78eae2f658e963bdeddf781ab732c5964ee1b5a9
Jin Dou <25721564+powerfulbean@users.noreply.github.com>
2023-08-16 11:55:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ import setuptools
 with open("./README.md", "r") as fh:
   long_description = fh.read()
 
+with open("StimRespFlow/__init__.py") as file:
+    for line in file.readlines():
+        m = re.match("__version__ *= *['\"](.*)['\"]", line)
+        if m:
+            version = m.group(1)
+
 setuptools.setup(
   name="StimRespFlow",
   version="2.1",
@@ -17,7 +23,7 @@ setuptools.setup(
   author_email="powerfulbean@gmail.com",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/StellarBlocks/StellarBrainwav",
+  url="https://github.com/powerfulbean/StimRespFlow",
   packages=setuptools.find_packages(),
   classifiers=[
   "Programming Language :: Python :: 3",
