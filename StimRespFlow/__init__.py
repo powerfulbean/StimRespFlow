6ab45b5dd0a1a5790feeb5799e19206bbe8cf077
Jin Dou <25721564+powerfulbean@users.noreply.github.com>
2024-08-21 16:09:25 -0400

    Update __init__.py

--- StimRespFlow/__init__.py
+++ StimRespFlow/__init__.py
@@ -2,4 +2,4 @@ from .Helper import StudyManage,StageControl
 from .Helper.StageControl import CStageControl 
 from .DataStruct.DataSet import CFlowDict
 
-__version__ = "2.1.6"
+__version__ = "2.1.7"
