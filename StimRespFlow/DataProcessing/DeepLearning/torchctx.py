16ed5b8f182643c36d6dae3a3375e6a36a6b891c
PowerfulBean <25721564+powerfulbean@users.noreply.github.com>
2024-08-28 15:59:02 -0400

    Update torchctx.py

--- StimRespFlow/DataProcessing/DeepLearning/torchctx.py
+++ StimRespFlow/DataProcessing/DeepLearning/torchctx.py
@@ -13,6 +13,26 @@ class MetricsLog:
     def __getitem__(self,key):
         return self.data[key]
 
+def result_default_transform(data):
+    return torch.stack(data, 0).mean(0)
+
+class Result:
+
+    def __init__(self, transform = result_default_transform):
+        self.data = {}
+        self.transform = transform
+    
+    def log(self, metricDict):
+        for k in metricDict:
+            if k not in self.data:
+                self.data[k] = []
+            self.data[k].append(metricDict[k])
+    
+    def __getitem__(self, key):
+        return self.transform(self.data[key])
+    
+
+
 class Context:
     def __init__(
         self,
@@ -29,6 +49,67 @@ class Context:
         self.metricslog = MetricsLog(metricsNames)
         self.folder = folder
         self.configs = configs
+        
+    def iter_dataloader(
+        self, 
+        dataloader,
+        func_forward,
+    ):
+        for batch in dataloader_train:
+            
+
+            #loss = []
+            self.optim.zero_grad()
+            model = self.model
+            for batch in dataloader_train:
+                model.train()
+                # print('batch shape', batch[0].shape, batch[1].shape)
+                # outputs = model(batch[0][0], batch[1][0])
+                outputs = func_forward(batch)
+                
+                # tempLoss = pearsonr_loss(outputs[0], outputs[1])
+                # tempLoss = tempLoss.sum()
+                # tempLoss.backward()
+                # optim.step()
+                func_backward(outputs)
+            
+            train_cache = Result(
+                lambda x: torch.stack(x, dim = 0).to('cpu').mean(0).detach()
+            )
+            
+            
+            with torch.no_grad():
+                for batch in dataloader:
+                    model.eval()
+                    outputs2 = model(batch[0][0], batch[1][0])
+                    # print(outputs[0].shape)
+                    tempR = Pearsonr(outputs2[0], outputs2[1])
+                    tempLoss = pearsonr_loss(outputs2[0], outputs2[1])
+                    # print(tempR, tempLoss)
+                    train_cache.log(
+                        {
+                            'train_loss': tempLoss,
+                            'train_r': tempR
+                        }
+                    )
+
+            val_cache = Result(
+                ['val_loss', 'val_r'], 
+                lambda x: torch.stack(x, dim = 0).to('cpu').mean(0).detach()
+            )
+            with torch.no_grad():
+                for batch in test_dataloader:
+                    model.eval()
+                    outputs2 = model(batch[0][0], batch[1][0])
+                    tempR = Pearsonr(outputs2[0], outputs2[1])
+                    tempLoss = pearsonr_loss(outputs2[0], outputs2[1])
+                    # print(tempR, tempLoss)
+                    val_cache.log(
+                        {
+                            'val_loss': tempLoss, 
+                            'val_r': tempR
+                        }
+                    )
     
 class SaveBest:
     def __init__(
