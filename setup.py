1092dc1b86dd481d5ee33f1a80eaf69891bc7069
PowerfulBean <25721564+powerfulbean@users.noreply.github.com>
2025-08-19 14:50:51 -0400

    rename

--- setup.py
+++ setup.py
@@ -11,20 +11,20 @@ import re
 with open("./README.md", "r") as fh:
   long_description = fh.read()
 
-with open("tray/__init__.py") as file:
+with open("tour/__init__.py") as file:
     for line in file.readlines():
         m = re.match("__version__ *= *['\"](.*)['\"]", line)
         if m:
             version = m.group(1)
 
 setuptools.setup(
-  name="turpan",
+  name="tour",
   version=version,
   author="Powerfulbean",
   author_email="powerfulbean@gmail.com",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/powerfulbean/turpan",
+  url="https://github.com/powerfulbean/pytour",
   packages=setuptools.find_packages(),
   classifiers=[
       "Programming Language :: Python :: 3",
